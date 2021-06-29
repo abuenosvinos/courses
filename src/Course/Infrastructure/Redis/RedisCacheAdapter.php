947d116b072f473b28a14fb2c2ad1f5a659125bd
Antonio Buenosvinos <antonio.buenosvinos@gmail.com>
2021-06-29 07:31:02 +0200

    feature; doctrine cache by redis

--- src/Course/Infrastructure/Redis/RedisCacheAdapter.php
+++ src/Course/Infrastructure/Redis/RedisCacheAdapter.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Course\Infrastructure\Redis;
 
+use App\Course\Domain\Entity\CourseCategory;
 use App\Course\Domain\Entity\CourseLevel;
 use Doctrine\Persistence\Event\LifecycleEventArgs;
 use Symfony\Contracts\Cache\TagAwareCacheInterface;
@@ -15,6 +16,11 @@ class RedisCacheAdapter
     public function __construct(TagAwareCacheInterface $doctrineResultCachePool)
     {
         $this->doctrineResultCachePool = $doctrineResultCachePool;
+    }
+
+    public function cleanCourseCategory(CourseCategory $courseCategory, LifecycleEventArgs $args)
+    {
+        $this->doctrineResultCachePool->delete(urlencode('[CourseCategory||searchAll][1]'));
     }
 
     public function cleanCourseLevel(CourseLevel $courseLevel, LifecycleEventArgs $args)
