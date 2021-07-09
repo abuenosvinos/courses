81d3ad0b8688cd566cfd6f80edb994716121b692
Antonio Buenosvinos <antonio.buenosvinos@gmail.com>
2021-07-09 18:17:56 +0200

    webpack updates

--- resources/js/menu.js
+++ resources/js/menu.js
@@ -1,3 +1,3 @@
 export default function menu() {
-    alert('b');
+    alert('q7');
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,18 @@
 const path = require('path');
 
 const appConfig = {
+    mode: process.env.NODE_ENV ? 'development' : 'production',
     entry: {
         app: './resources/js/app.js'
     },
     output: {
         path: path.resolve(__dirname, 'public/dist/app'),
         filename: '[name].js',
-    },
+    }
 };
 
 const adminConfig = {
+    mode: process.env.NODE_ENV ? 'development' : 'production',
     entry: {
         app: './resources/js/admin.js'
     },
@@ -20,4 +22,7 @@ const adminConfig = {
     },
 };
 
-module.exports = [appConfig, adminConfig];
+module.exports = [
+    appConfig,
+    adminConfig
+];
