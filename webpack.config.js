aa9636d63c905e8a02a446f47ccd30a49a9459cb
Antonio Buenosvinos <antonio.buenosvinos@gmail.com>
2021-07-12 17:26:31 +0200

    add babel to transpile

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,29 @@
 const path = require('path');
 
 const appConfig = {
-    mode: process.env.NODE_ENV ? 'development' : 'production',
+    mode: process.env.NODE_ENV,
+    devtool: process.env.NODE_ENV === 'production' ? 'source-map' : 'eval',
     entry: {
         app: './resources/js/app.js'
     },
     output: {
         path: path.resolve(__dirname, 'public/dist/app'),
         filename: '[name].js',
-    }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: ['babel-loader'],
+            },
+        ],
+    },
 };
 
 const adminConfig = {
-    mode: process.env.NODE_ENV ? 'development' : 'production',
+    mode: process.env.NODE_ENV,
+    devtool: process.env.NODE_ENV === 'production' ? 'source-map' : 'eval',
     entry: {
         app: './resources/js/admin.js'
     },
@@ -20,9 +31,19 @@ const adminConfig = {
         path: path.resolve(__dirname, 'public/dist/admin'),
         filename: '[name].js',
     },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: ['babel-loader'],
+            },
+        ],
+    },
 };
 
 module.exports = [
     appConfig,
     adminConfig
 ];
+module.exports.parallelism = 2;
